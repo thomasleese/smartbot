2137f166b5af5af64b18dbc0ed42d7c9f0094bea
Tom Leese <leese.thomas81@gmail.com>
2014-01-12 16:25:21 +0000

    Add google plugin

---
+++ plugins/google.py
@@ -0,0 +1,29 @@
+import requests
+import urllib.parse
+
+class Plugin:
+    def __init__(self, key, cx):
+        self.key = key
+        self.cx = cx
+
+    def __call__(self, bot):
+        bot.on_respond(r"(?:google|search)(?: for)? (.*)$", self.on_respond)
+        bot.on_help("google", self.on_help)
+
+    def on_respond(self, bot, msg, reply):
+        url = "https://www.googleapis.com/customsearch/v1?key={0}&cx={1}&q={2}".format(
+            urllib.parse.quote(self.key),
+            urllib.parse.quote(self.cx),
+            urllib.parse.quote(msg["match"].group(1))
+        )
+        headers = { "User-Agent": "SmartBot" }
+
+        res = requests.get(url, headers=headers).json()
+        if res["items"]:
+            for i, item in enumerate(res["items"][:5]):
+                reply("[{0}]: {1}".format(i, item["link"]))
+        else:
+            reply("No results!")
+
+    def on_help(self, bot, msg, reply):
+        reply("Syntax: google|search [for] <query>")
