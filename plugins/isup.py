02f207269f7d2773919e520e04ab8f9261357d4b
Tom Leese <leese.thomas81@gmail.com>
2014-01-12 16:25:34 +0000

    Add isup plugin

---
+++ plugins/isup.py
@@ -0,0 +1,21 @@
+import requests
+import urllib.parse
+
+class Plugin:
+    def __call__(self, bot):
+        bot.on_respond(r"is (.*) (up|down)(\?)?", self.on_respond)
+        bot.on_respond(r"isup (.*)$", self.on_respond)
+        bot.on_help("isup", self.on_help)
+
+    def on_respond(self, bot, msg, reply):
+        url = "http://isitup.org/" + urllib.parse.quote(msg["match"].group(1)) + ".json"
+        headers = { "User-Agent": "SmartBot" }
+
+        res = requests.get(url, headers=headers).json()
+        if res["status_code"] == 1:
+            reply("{0} looks up for me.".format(res["domain"]))
+        else:
+            reply("{0} looks down for me.".format(res["domain"]))
+
+    def on_help(self, bot, msg, reply):
+        reply("Syntax: is <domain> up|down")
