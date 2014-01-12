65c7cac5672cade554f897c6fa8f8f38ebcd3a44
Tom Leese <leese.thomas81@gmail.com>
2014-01-12 16:24:59 +0000

    Add excuses plugin

---
+++ plugins/excuses.py
@@ -0,0 +1,19 @@
+import lxml.html
+import requests
+
+class Plugin:
+    def __call__(self, bot):
+        bot.on_respond(r"(developer('s|s)? |programmer('s|s)? )?excuse( me)?", self.on_respond)
+        bot.on_help("excuses", self.on_help)
+
+    def on_respond(self, bot, msg, reply):
+        page = requests.get("http://developerexcuses.com/")
+        tree = lxml.html.fromstring(page.text)
+        thing = tree.cssselect(".wrapper a")[0].text_content()
+        if thing:
+            reply(thing)
+        else:
+            reply("You have no excuse!")
+
+    def on_help(self, bot, msg, reply):
+        reply("Syntax: [developer[']s|programmer[']s] excuse [me]")
