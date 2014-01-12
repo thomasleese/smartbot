b7a20acb0dc061c6bd16165e2f744b6416ac34f1
Tom Leese <leese.thomas81@gmail.com>
2014-01-12 16:24:25 +0000

    Add coin plugin

---
+++ plugins/coin.py
@@ -0,0 +1,13 @@
+import random
+
+class Plugin:
+    def __call__(self, bot):
+        bot.on_respond(r"(throw|flip|toss)( a)? coin", self.on_respond)
+        bot.on_help("coin", self.on_help)
+
+    def on_respond(self, msg, reply):
+        reply(random.choice([ "Heads", "Tails" ]))
+
+    def on_help(self, bot, msg, reply):
+        reply("Throws coins.")
+        reply("Syntax: throw|flip|toss [a] coin")
