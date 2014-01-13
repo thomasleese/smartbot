24cd8de73b18ff55f2e5ce229f7e4d7c00fbe6ff
Tom Leese <leese.thomas81@gmail.com>
2014-01-13 11:49:29 +0000

    Add decide plugin

---
+++ plugins/decide.py
@@ -0,0 +1,16 @@
+import random
+
+class Plugin:
+    def __call__(self, bot):
+        bot.on_respond(r"decide \"(.*)\"$", self.on_respond_1)
+        bot.on_respond(r"decide ([^\"]+)$", self.on_respond_2)
+        bot.on_help("decide", self.on_help)
+
+    def on_respond_1(self, bot, msg, reply):
+        reply("I pick {0}.".format(random.choice(msg["match"].group(1).split("\" \""))))
+
+    def on_respond_2(self, bot, msg, reply):
+        reply("I pick {0}.".format(random.choice(msg["match"].group(1).split(" "))))
+
+    def on_help(self, bot, msg, reply):
+        reply("Syntax: decide \"option\" …")
