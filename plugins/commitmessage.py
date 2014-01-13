97a66e22f26a6d49af8260c90fe430268b5f687c
Tom Leese <leese.thomas81@gmail.com>
2014-01-13 11:49:07 +0000

    Add commitmessage plugin

---
+++ plugins/commitmessage.py
@@ -0,0 +1,16 @@
+import requests
+
+class Plugin:
+    def __call__(self, bot):
+        bot.on_respond(r"commit message", self.on_respond)
+        bot.on_help("commitmessage", self.on_help)
+
+    def on_respond(self, bot, msg, reply):
+        page = requests.get("http://whatthecommit.com/index.txt")
+        if page.text:
+            reply(page.text)
+        else:
+            reply("No Commit Message found.")
+
+    def on_help(self, bot, msg, reply):
+        reply("Syntax: commit message")
