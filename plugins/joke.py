01a21359ccf9bb0759895038b750cafb64e6d3f3
Tom Leese <leese.thomas81@gmail.com>
2014-01-15 08:11:02 +0000

    Add joke plugin

---
+++ plugins/joke.py
@@ -0,0 +1,20 @@
+import requests
+import time
+import urllib.parse
+
+class Plugin:
+    def __call__(self, bot):
+        bot.on_respond(r"(?:give me a |gimme a |gimme )joke$", self.on_respond)
+        bot.on_help("joke", self.on_help)
+
+    def on_respond(self, bot, msg, reply):
+        url = "http://jokels.com/random_joke"
+        headers = { "User-Agent": "SmartBot" }
+
+        res = requests.get(url, headers=headers).json()
+        reply(res["joke"]["question"])
+        time.sleep(2)
+        reply(res["joke"]["answer"])
+
+    def on_help(self, bot, reply):
+        reply("Syntax: [give me a] joke")
