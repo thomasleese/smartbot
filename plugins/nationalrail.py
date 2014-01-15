eed6b1170db51abb71b97fc8218a758988d2b7dd
Tom Leese <leese.thomas81@gmail.com>
2014-01-15 08:11:17 +0000

    Add nationalrail plugin

---
+++ plugins/nationalrail.py
@@ -0,0 +1,27 @@
+import requests
+import urllib.parse
+
+class Plugin:
+    def __call__(self, bot):
+        bot.on_respond(r"trains (\w{3}) to (\w{3})$", self.on_respond)
+        bot.on_help("nationalrail", self.on_help)
+
+    def on_respond(self, bot, msg, reply):
+        url = "http://ojp.nationalrail.co.uk/service/ldb/liveTrainsJson?departing=true&liveTrainsFrom={0}&liveTrainsTo={1}".format(
+            urllib.parse.quote(msg["match"][0][0]),
+            urllib.parse.quote(msg["match"][0][1])
+        )
+        headers = { "User-Agent": "SmartBot" }
+
+        res = requests.get(url, headers=headers).json()
+        if res["trains"]:
+            for i, train in enumerate(res["trains"]):
+                if train[4]:
+                    reply("[{0}]: the {1} to {2} on platform {3} ({4}).".format(i, train[1], train[2], train[4], train[3].lower()))
+                else:
+                    reply("[{0}]: the {1} to {2} ({3}).".format(i, train[1], train[2], train[3].lower()))
+        else:
+            reply("No trains.")
+
+    def on_help(self, bot, msg, reply):
+        reply("Syntax: trains <from> to <to>")
