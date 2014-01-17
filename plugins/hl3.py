9d02604e3b36dea67d0a701e1b4c5c191fad0409
Tom Leese <leese.thomas81@gmail.com>
2014-01-17 19:21:18 +0000

    Fix hl3 plugin help

--- plugins/hl3.py
+++ plugins/hl3.py
@@ -11,10 +11,11 @@ responses = [
 class Plugin:
     def __call__(self, bot):
         bot.on_respond(r"(is )?(hl3|half-life 3|half life 3|hl 3)( out( yet)?)?(\?)?", self.on_respond)
-        bot.on_help("humble", self.on_help)
+        bot.on_help("hl3", self.on_help)
 
     def on_respond(self, bot, msg, reply):
         reply(random.choice(responses))
 
     def on_help(self, bot, msg, reply):
-        reply("Syntax: [is] hl3 [out] [yet]")
+        reply("Is Half-Life 3 out?")
+        reply("Syntax: [is] hl3 [out [yet]]")
