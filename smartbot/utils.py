58d3cec13121a0df969dc8301b2ee83b9b310087
Tom Leese <leese.thomas81@gmail.com>
2014-01-17 16:41:35 +0000

    Remove newlines from website titles

--- smartbot/utils.py
+++ smartbot/utils.py
@@ -71,7 +71,7 @@ def get_website_title(url):
         if page.status_code == 200 and page.headers.get("Content-Type", "").startswith("text/html"):
             tree = lxml.html.fromstring(page.content)
             title = tree.cssselect("title")[0].text_content()
-            return title.strip()
+            return title.strip().replace("\n", "").replace("\r", "")
     except requests.exceptions.Timeout:
         return "Timeout!"
     except IndexError: # no title element
