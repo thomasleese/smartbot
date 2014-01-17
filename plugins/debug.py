08dcf4de96c6f8890e467e4d17c9449bae8064be
Tom Leese <leese.thomas81@gmail.com>
2014-01-17 00:00:47 +0000

    Add say alias to echo

--- plugins/debug.py
+++ plugins/debug.py
@@ -3,7 +3,7 @@ import time
 class Plugin:
     def __call__(self, bot):
         bot.on_respond(r"ping$", lambda bot, msg, reply: reply("PONG"))
-        bot.on_respond(r"echo (.*)$", lambda bot, msg, reply: reply(msg["match"][0]))
+        bot.on_respond(r"(?:echo|say) (.*)$", lambda bot, msg, reply: reply(msg["match"][0]))
         bot.on_respond(r"time$", lambda bot, msg, reply: reply(time.time()))
         bot.on_help("debug", self.on_help)
 
