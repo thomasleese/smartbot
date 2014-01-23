fa01afb22b6dde5a828fe727aa178b3ba1e7fca3
Tom Leese <leese.thomas81@gmail.com>
2014-01-23 17:50:58 +0000

    Add support for single-quotes in decide plugin

--- plugins/decide.py
+++ plugins/decide.py
@@ -3,13 +3,17 @@ import random
 class Plugin:
     def __call__(self, bot):
         bot.on_respond(r"decide \"(.*)\"$", self.on_respond_1)
-        bot.on_respond(r"decide ([^\"]+)$", self.on_respond_2)
+        bot.on_respond(r"decide '(.*)'$", self.on_respond_2)
+        bot.on_respond(r"decide ([^\"]+)$", self.on_respond_3)
         bot.on_help("decide", self.on_help)
 
     def on_respond_1(self, bot, msg, reply):
         reply("I pick {0}.".format(random.choice(msg["match"][0].split("\" \""))))
 
     def on_respond_2(self, bot, msg, reply):
+        reply("I pick {0}.".format(random.choice(msg["match"][0].split("' '"))))
+
+    def on_respond_3(self, bot, msg, reply):
         reply("I pick {0}.".format(random.choice(msg["match"][0].split(" "))))
 
     def on_help(self, bot, msg, reply):
