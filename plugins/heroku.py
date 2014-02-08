778bd6afa271658da44b396398c0c9fd79c15a95
Tom Leese <leese.thomas81@gmail.com>
2014-02-08 09:45:32 +0000

    Add heroku plugin

---
+++ plugins/heroku.py
@@ -0,0 +1,22 @@
+import requests
+import sys
+
+
+class Plugin:
+    def on_command(self, bot, msg):
+        if len(sys.argv) >= 2:
+            action = sys.argv[1]
+            if "status".startswith(action):
+                url = "https://status.heroku.com/api/v3/current-status"
+                headers = {"User-Agent": "SmartBot"}
+
+                res = requests.get(url, headers=headers).json()
+                print("Production: {0}".format(res["status"]["Production"]))
+                print("Development: {0}".format(res["status"]["Development"]))
+            else:
+                print("No such action:", action)
+        else:
+            print(self.on_help(bot))
+
+    def on_help(self, bot):
+        return "Usage: heroku status"
