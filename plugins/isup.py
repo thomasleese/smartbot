8576c6ed9738ad8e9f1e563d0fe0b056c3b58d9b
Tom Leese <leese.thomas81@gmail.com>
2014-02-11 15:37:29 +0000

    Add isup plugin

---
+++ plugins/isup.py
@@ -0,0 +1,24 @@
+import requests
+import urllib.parse
+import sys
+
+
+class Plugin:
+    def on_command(self, bot, msg):
+        url = None
+        if len(sys.argv) >= 2:
+            url = sys.argv[1]
+        else:
+            url = sys.stdin.read().strip()
+
+        url = "http://isitup.org/{0}.json".format(urllib.parse.quote(url))
+        headers = {"User-Agent": "SmartBot"}
+
+        res = requests.get(url, headers=headers).json()
+        if res["status_code"] == 1:
+            print("{0} looks up for me.".format(res["domain"]))
+        else:
+            print("{0} looks down for me.".format(res["domain"]))
+
+    def on_help(self, bot):
+        return "Usage: isup <domain>"
