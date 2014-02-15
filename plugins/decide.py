e7c47f4128cc5d133072e35c6c0fd6f2668e7b43
Tom Leese <leese.thomas81@gmail.com>
2014-02-15 14:01:45 +0000

    Change decide plugin to use shlex

--- plugins/decide.py
+++ plugins/decide.py
@@ -1,20 +1,15 @@
 import random
+import shlex
+
 
 class Plugin:
     def __call__(self, bot):
-        bot.on_respond(r"decide \"(.*)\"$", self.on_respond_1)
-        bot.on_respond(r"decide '(.*)'$", self.on_respond_2)
-        bot.on_respond(r"decide ([^\"']+)$", self.on_respond_3)
+        bot.on_respond(r"decide (.*)$", self.on_respond)
         bot.on_help("decide", self.on_help)
 
-    def on_respond_1(self, bot, msg, reply):
-        reply("I pick {0}.".format(random.choice(msg["match"][0].split("\" \""))))
-
-    def on_respond_2(self, bot, msg, reply):
-        reply("I pick {0}.".format(random.choice(msg["match"][0].split("' '"))))
-
-    def on_respond_3(self, bot, msg, reply):
-        reply("I pick {0}.".format(random.choice(msg["match"][0].split(" "))))
+    def on_respond(self, bot, msg, reply):
+        choices = shlex.split(msg["match"][0])
+        reply("I pick {0}.".format(random.choice(choices)))
 
     def on_help(self, bot, msg, reply):
-        reply("Syntax: decide \"option\" …")
+        reply("Syntax: decide 'option' …")
