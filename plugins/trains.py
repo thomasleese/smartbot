1536d5349569bd9a216f464d9b23fc1bca217297
Tom Leese <leese.thomas81@gmail.com>
2014-02-22 19:51:43 +0000

    Add trains plugin

---
+++ plugins/trains.py
@@ -0,0 +1,29 @@
+import requests
+import urllib.parse
+import sys
+
+
+class Plugin:
+    def on_command(self, bot, msg):
+        if len(sys.argv) >= 3:
+            url = "http://ojp.nationalrail.co.uk/service/ldb/liveTrainsJson?departing=true&liveTrainsFrom={0}&liveTrainsTo={1}".format(
+                urllib.parse.quote(sys.argv[1]),
+                urllib.parse.quote(sys.argv[2])
+            )
+            headers = {"User-Agent": "SmartBot"}
+
+            res = requests.get(url, headers=headers).json()
+            if res["trains"]:
+                for i, train in enumerate(res["trains"]):
+                    if train[4]:
+                        print("[{0}]: the {1} to {2} on platform {3} ({4}).".format(i, train[1], train[2], train[4],
+                                                                                    train[3].lower()))
+                    else:
+                        print("[{0}]: the {1} to {2} ({3}).".format(i, train[1], train[2], train[3].lower()))
+            else:
+                print("No trains.")
+        else:
+            print(self.on_help(bot))
+
+    def on_help(self, bot):
+        return "Usage: trains <from> <to>"
