ad40d10f8b996eec2ff7a2ef3332fe025168bf46
Tom Leese <leese.thomas81@gmail.com>
2014-02-22 10:28:47 +0000

    Add seen plugin

---
+++ plugins/seen.py
@@ -0,0 +1,25 @@
+import datetime
+import sys
+
+
+class Plugin:
+    def on_message(self, bot, msg):
+        bot.storage["seen." + msg["sender"]] = {
+            "action": "spoke",
+            "datetime": datetime.datetime.now()
+        }
+
+    def on_command(self, bot, msg):
+        if len(sys.argv) >= 2:
+            user = sys.argv[1]
+            try:
+                info = bot.storage["seen." + user]
+                print("{0} {1} on {2}.".format(user, info["action"],
+                                               info["datetime"].strftime("%a %d %b %H:%M %Z").strip()))
+            except KeyError:
+                print("I don't know anything about {0}.".format(user))
+        else:
+            print(self.on_help(bot))
+
+    def on_help(self, bot):
+        return "Usage: seen <user>"
