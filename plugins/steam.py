b1d0c13c35281e265f7e5214262f444eee9fe568
Tom Leese <leese.thomas81@gmail.com>
2014-02-22 14:53:27 +0000

    Add steam plugin

---
+++ plugins/steam.py
@@ -0,0 +1,31 @@
+import lxml.html
+import requests
+import sys
+
+from smartbot import utils
+
+
+class Plugin:
+    def on_command(self, bot, msg):
+        if len(sys.argv) >= 2:
+            action = sys.argv[1]
+            if "deal".startswith(action):
+                page = requests.get("http://store.steampowered.com")
+                tree = lxml.html.fromstring(page.text)
+                if tree.cssselect(".dailydeal"):
+                    url = tree.cssselect(".dailydeal a")[0].get("href")
+                    original_price = tree.cssselect(".dailydeal_content .discount_original_price")[0].text
+                    final_price = tree.cssselect(".dailydeal_content .discount_final_price")[0].text
+                    print("{0} - {1} - from {2} to {3}".format(url,
+                                                               utils.web.get_title(url),
+                                                               original_price,
+                                                               final_price))
+                else:
+                    print("No daily deal.")
+            else:
+                print("No such action:", action)
+        else:
+            print(self.on_help(bot))
+
+    def on_help(self, bot):
+        return "Usage: steam deal"
