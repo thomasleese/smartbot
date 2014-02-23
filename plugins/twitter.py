404b8e7c919506a5ca7a1410f31e14640e6d28b1
Tom Leese <leese.thomas81@gmail.com>
2014-02-23 11:24:33 +0000

    Add twitter plugin

---
+++ plugins/twitter.py
@@ -0,0 +1,24 @@
+import twython
+import sys
+
+
+class Plugin:
+    def __init__(self, consumer_key, consumer_secret, access_token, access_secret):
+        self.twitter = twython.Twython(consumer_key, consumer_secret, access_token, access_secret)
+
+    def on_command(self, bot, msg):
+        person = None
+        if len(sys.argv) >= 2:
+            person = sys.argv[1]
+        else:
+            person = sys.stdin.read().strip()
+
+        if person:
+            tweets = self.twitter.get_user_timeline(screen_name=person)
+            for i, tweet in enumerate(tweets[:3]):
+                print("[{0}]: {1}".format(i, tweet["text"]))
+        else:
+            print(self.on_help(bot))
+
+    def on_help(self, bot):
+        return "Usage: twitter <person>"
