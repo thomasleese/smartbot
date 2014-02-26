57e7be79588fade1f77125bc79537ea18a307c92
Tom Leese <leese.thomas81@gmail.com>
2014-02-26 15:19:58 +0000

    Change remind plugin date output
    No need for it to be so precise.

--- plugins/remind.py
+++ plugins/remind.py
@@ -12,17 +12,19 @@ class Plugin:
         bot.send(msg["reply_to"], message)
 
     def on_respond(self, bot, msg, reply):
-        try:
-            match = re.match(r"remind me (to|about|that) (.*) (in|at) (.*)$", msg["message"], re.IGNORECASE)
-            if match:
+        match = re.match(r"remind me (to|about|that) (.*) (in|at) (.*)$", msg["message"], re.IGNORECASE)
+        if match:
+            try:
                 date = utils.datetime.parse("{0} {1}".format(match.group(3), match.group(4)))
-                reply("Sure thing {0}, I'll remind you at {1}.".format(msg["sender"], date))
+            except ValueError:
+                reply("I don't understand that date.")
+            else:
+                reply("Sure thing {0}, I'll remind you on {1}.".format(msg["sender"], date.strftime("%c")))
                 message = "{0}: you asked me to remind you {1} {2}".format(msg["sender"], match.group(1), match.group(2))
+
                 duration = max(0, (date - datetime.datetime.now()).total_seconds())
                 t = threading.Thread(target=self.on_timeout, args=(message, duration, bot, msg))
                 t.start()
-        except ValueError:
-            reply("I don't understand that date.")
 
     def on_help(self):
         return "Usage: remind me to|about|that <something> in|at <time>"
