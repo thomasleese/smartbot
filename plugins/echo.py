871e305ab44a6fa3d1df62e82ac12c81c1e8716b
Tom Leese <leese.thomas81@gmail.com>
2014-03-01 16:17:59 +0000

    Add tests for echo plugin

--- plugins/echo.py
+++ plugins/echo.py
@@ -1,9 +1,25 @@
-import sys
+import io
+import unittest
 
 
 class Plugin:
-    def on_command(self, bot, msg):
-        print(*sys.argv[1:])
+    def on_command(self, bot, msg, stdin, stdout, reply):
+        print(*msg["args"][1:], file=stdout)
 
     def on_help(self):
         return "Usage: echo <string>"
+
+
+class Test(unittest.TestCase):
+    def setUp(self):
+        self.plugin = Plugin()
+
+    def test_words(self):
+        for i in range(1, 100):
+            stdout = io.StringIO()
+            words = [None] + ["word"] * i
+            self.plugin.on_command(None, {"args": words}, None, stdout, None)
+            self.assertEqual(stdout.getvalue().strip(), " ".join(words[1:]))
+
+    def test_help(self):
+        self.assertTrue(self.plugin.on_help())
