952041573336652931003dd8b48aa82f5f0cb3da
Tom Leese <leese.thomas81@gmail.com>
2014-03-01 14:12:21 +0000

    Add tests to autojoin plugin

--- plugins/autojoin.py
+++ plugins/autojoin.py
@@ -1,3 +1,5 @@
+import unittest
+
 class Plugin:
     def __init__(self, channels):
         self.channels = channels
@@ -8,3 +10,22 @@ class Plugin:
 
     def on_help(self):
         return "Automatically joins channels when the bot connects."
+
+
+class Test(unittest.TestCase):
+    class ExampleBot:
+        def __init__(self, test, channel):
+            self.test = test
+            self.channel = channel
+
+        def join(self, channel):
+            self.test.assertEqual(channel, self.channel)
+
+    def setUp(self):
+        self.plugin = Plugin(["#test"])
+
+    def test_join(self):
+        self.plugin.on_ready(Test.ExampleBot(self, "#test"))
+
+    def test_help(self):
+        self.assertTrue(self.plugin.on_help())
