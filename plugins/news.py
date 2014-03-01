e6a3af39ac1ec476b905a4586e889f5e281e45f3
Tom Leese <leese.thomas81@gmail.com>
2014-03-01 23:21:30 +0000

    Add tests to new plugin

--- plugins/news.py
+++ plugins/news.py
@@ -1,14 +1,14 @@
+import io
 import requests
+import unittest
 import urllib.parse
-import sys
-
 
 
 class Plugin:
-    def on_command(self, bot, msg):
-        topic = " ".join(sys.argv[1:])
+    def on_command(self, bot, msg, stdin, stdout, reply):
+        topic = " ".join(msg["args"][1:])
         if not topic:
-            topic = sys.stdin.read().strip()
+            topic = stdin.read().strip()
 
         if topic:
             url = "https://ajax.googleapis.com/ajax/services/search/news?v=1.0&rsz=5&q={0}".format(
@@ -25,9 +25,27 @@ class Plugin:
             for i, story in enumerate(stories):
                 title = story["titleNoFormatting"].replace("&#39;", "'").replace("`", "'").replace("&quot;", "\"")
                 link = story["unescapedUrl"]
-                print("[{0}]: {1} - {2}".format(i, title, link))
+                print("[{0}]: {1} - {2}".format(i, title, link), file=stdout)
         else:
-            print("No news stories.")
+            print("No news stories.", file=stdout)
 
     def on_help(self):
         return "Syntax: news [<topic>]"
+
+
+class Test(unittest.TestCase):
+    def setUp(self):
+        self.plugin = Plugin()
+
+    def test_no_category(self):
+        stdout = io.StringIO()
+        self.plugin.on_command(None, {"args": [None]}, stdout, stdout, None)
+        self.assertNotEqual("No news stories.", stdout.getvalue().strip())
+
+    def test_category(self):
+        stdout = io.StringIO()
+        self.plugin.on_command(None, {"args": [None, "cats"]}, stdout, stdout, None)
+        self.assertNotEqual("No news stories.", stdout.getvalue().strip())
+
+    def test_help(self):
+        self.assertTrue(self.plugin.on_help())
