dc241c6ddf0f07d6fc372b863d9ab5ab13bfed5c
Tom Leese <leese.thomas81@gmail.com>
2014-03-01 22:38:36 +0000

    Add tests for heroku plugin

--- plugins/heroku.py
+++ plugins/heroku.py
@@ -1,22 +1,42 @@
+import io
 import requests
-import sys
+import unittest
 
 
 class Plugin:
-    def on_command(self, bot, msg):
-        if len(sys.argv) >= 2:
-            action = sys.argv[1]
+    def on_command(self, bot, msg, stdin, stdout, reply):
+        if len(msg["args"]) >= 2:
+            action = msg["args"][1]
             if "status".startswith(action):
                 url = "https://status.heroku.com/api/v3/current-status"
                 headers = {"User-Agent": "SmartBot"}
 
                 res = requests.get(url, headers=headers).json()
-                print("Production: {0}".format(res["status"]["Production"]))
-                print("Development: {0}".format(res["status"]["Development"]))
+                print("Production: {0}".format(res["status"]["Production"]), file=stdout)
+                print("Development: {0}".format(res["status"]["Development"]), file=stdout)
             else:
-                print("No such action:", action)
+                print("No such action:", action, file=stdout)
         else:
-            print(self.on_help())
+            print(self.on_help(), file=stdout)
 
     def on_help(self):
         return "Usage: heroku status"
+
+
+class Test(unittest.TestCase):
+    def setUp(self):
+        self.plugin = Plugin()
+
+    def test_status(self):
+        stdout = io.StringIO()
+        self.plugin.on_command(None, {"args": [None, "status"]}, None, stdout, None)
+        self.assertFalse(stdout.getvalue().startswith("No such action"))
+        self.assertNotEqual(self.plugin.on_help(), stdout.getvalue().strip())
+
+    def test_help(self):
+        self.assertTrue(self.plugin.on_help())
+
+    def test_no_args(self):
+        stdout = io.StringIO()
+        self.plugin.on_command(None, {"args": [None]}, stdout, stdout, None)
+        self.assertEqual(self.plugin.on_help(), stdout.getvalue().strip())
