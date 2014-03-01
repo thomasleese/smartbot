dde62a1bc100ba4be16489e4171f0c78593d95af
Tom Leese <leese.thomas81@gmail.com>
2014-03-01 22:56:04 +0000

    Add tests to http plugin

--- plugins/http.py
+++ plugins/http.py
@@ -1,21 +1,44 @@
+import io
 import requests
-import sys
+import unittest
 
 
 class Plugin:
-    def on_command(self, bot, msg):
+    def on_command(self, bot, msg, stdin, stdout, reply):
         url = None
-        if len(sys.argv) >= 3:
-            url = sys.argv[2]
+        if len(msg["args"]) >= 3:
+            url = msg["args"][2]
         else:
-            url = sys.stdin.read().strip()
+            url = stdin.read().strip()
 
         if url:
             headers = {"User-Agent": "SmartBot"}
             page = requests.get(url, headers=headers)
-            print(page.text)
+            print(page.text, file=stdout)
         else:
-            print(self.on_help())
+            print(self.on_help(), file=stdout)
 
     def on_help(self):
         return "Usage: http get <url>"
+
+
+class Test(unittest.TestCase):
+    def setUp(self):
+        self.plugin = Plugin()
+
+    def test_search(self):
+        stdout = io.StringIO()
+        self.plugin.on_command(None, {"args": [None, "get", "http://google.com"]}, None, stdout, None)
+        self.assertNotEqual(self.plugin.on_help(), stdout.getvalue().strip())
+
+    def test_help(self):
+        self.assertTrue(self.plugin.on_help())
+
+    def test_no_args(self):
+        stdout = io.StringIO()
+        self.plugin.on_command(None, {"args": [None]}, stdout, stdout, None)
+        self.assertEqual(self.plugin.on_help(), stdout.getvalue().strip())
+
+        stdout = io.StringIO()
+        self.plugin.on_command(None, {"args": [None, None]}, stdout, stdout, None)
+        self.assertEqual(self.plugin.on_help(), stdout.getvalue().strip())
