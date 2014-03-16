8e212ba847585e0f6471d2aa49da8b832cd08501
Tom Leese <tom@tomleese.me.uk>
2014-03-16 18:48:53 +0000

    Add output formatting for ANSI terminals

--- smartbot/backend.py
+++ smartbot/backend.py
@@ -3,6 +3,8 @@ import socket
 import threading
 import time
 
+from .formatting import *
+
 
 class _Backend:
     def __init__(self):
@@ -164,4 +166,45 @@ class CommandLine(_Backend):
         print(target + ":", message)
 
     def format(self, text, properties):
-        return text
+        values = []
+
+        if Style.bold in properties:
+            values.append("1")
+        if Style.italic in properties:
+            values.append("3")
+        if Style.underline in properties:
+            values.append("4")
+        if Colour.fg_white in properties:
+            values.append("37")
+        if Colour.fg_black in properties:
+            values.append("30")
+        if Colour.fg_blue in properties:
+            values.append("34")
+        if Colour.fg_green in properties:
+            values.append("32")
+        if Colour.fg_red in properties:
+            values.append("31")
+        if Colour.fg_purple in properties:
+            values.append("35")
+        if Colour.fg_yellow in properties:
+            values.append("33")
+        if Colour.fg_light_cyan in properties:
+            values.append("36")
+        if Colour.bg_white in properties:
+            values.append("47")
+        if Colour.bg_black in properties:
+            values.append("40")
+        if Colour.bg_blue in properties:
+            values.append("44")
+        if Colour.bg_green in properties:
+            values.append("42")
+        if Colour.bg_red in properties:
+            values.append("41")
+        if Colour.bg_purple in properties:
+            values.append("45")
+        if Colour.bg_yellow in properties:
+            values.append("43")
+        if Colour.bg_light_cyan in properties:
+            values.append("46")
+
+        return "\033[{}m{}\033[0m".format(";".join(values), text)
--- smartbot/formatting.py
+++ smartbot/formatting.py
@@ -1,22 +1,38 @@
-class Colour:
-    white = 0
-    black = 1
-    blue = 2
-    green = 3
-    red = 4
-    brown = 5
-    purple = 6
-    orange = 7
-    yellow = 8
-    light_green = 9
-    teal = 10
-    light_cyan = 11
-    light_blue = 12
-    pink = 13
-    grey = 14
-    light_grey = 15
-
 class Style:
     bold = 0
-    italics = 1
+    italic = 1
     underline = 2
+
+class Colour:
+    fg_white = 3
+    fg_black = 4
+    fg_blue = 5
+    fg_green = 6
+    fg_red = 7
+    fg_brown = 8
+    fg_purple = 9
+    fg_orange = 10
+    fg_yellow = 11
+    fg_light_green = 12
+    fg_teal = 13
+    fg_light_cyan = 14
+    fg_light_blue = 15
+    fg_pink = 16
+    fg_grey = 17
+    fg_light_grey = 18
+    bg_white = 19
+    bg_black = 20
+    bg_blue = 21
+    bg_green = 22
+    bg_red = 23
+    bg_brown = 24
+    bg_purple = 25
+    bg_orange = 26
+    bg_yellow = 27
+    bg_light_green = 28
+    bg_teal = 29
+    bg_light_cyan = 30
+    bg_light_blue = 31
+    bg_pink = 32
+    bg_grey = 33
+    bg_light_grey = 34
