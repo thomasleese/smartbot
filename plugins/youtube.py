6788e86c3302d26165fc8bd78c2e224f8a5bf042
Tom Leese <tom@tomleese.me.uk>
2014-03-25 23:22:02 +0000

    Add youtube plugin

---
+++ plugins/youtube.py
@@ -0,0 +1,37 @@
+import re
+import requests
+import urllib
+
+from smartbot import utils
+from smartbot.formatting import *
+
+
+class Plugin:
+    def __init__(self, key):
+        self.key = key
+
+    def on_message(self, bot, msg, reply):
+        match = re.findall(r"https?://(?:www\.)youtube\.com/watch\?v=([^\s]+)", msg["message"], re.IGNORECASE)
+        for i, id in enumerate(match):
+            url = "https://www.googleapis.com/youtube/v3/videos?key={}&part=contentDetails%2Csnippet%2Cstatistics&id={}".format(
+                urllib.parse.quote(self.key),
+                urllib.parse.quote(id)
+            )
+            print(url)
+            headers = {"User-Agent": "SmartBot"}
+
+            res = requests.get(url, headers=headers).json()
+            if res["items"]:
+                video = res["items"][0]
+                thumbnail = video["snippet"]["thumbnails"]["default"]["url"]
+                duration = video["contentDetails"]["duration"]
+                views = video["statistics"]["viewCount"]
+                likes = video["statistics"]["likeCount"]
+                reply("{}: ⌚ {} 👀 {} 👍 {} {}".format(bot.format("[{}]".format(i), Style.bold),
+                                                      duration,
+                                                      views,
+                                                      likes,
+                                                      bot.format(thumbnail, Colour.fg_grey)))
+
+    def on_help(self):
+        return "Sends information about YouTube URLs posted."
