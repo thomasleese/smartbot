c7c9aee2642be7f1abf5651e71d0db14751974b7
Tom Leese <tom@tomleese.me.uk>
2014-05-18 13:48:16 +0100

    Add pastebin plugin

---
+++ plugins/pastebin.py
@@ -0,0 +1,18 @@
+import io
+import re
+import unittest
+
+from smartbot import utils
+
+
+class Plugin:
+    names = ["pastebin.com", "badpastebin"]
+
+    def on_command(self, bot, msg, stdin, stdout, reply):
+        if msg["args"][0] == "pastebin.com":
+            reply("Don't use it. Use some sane pastebin like bpaste.net, gist.github.com, sprunge.us or ix.io. Also see !badpastebin")
+        elif msg["args"][0] == "badpastebin":
+            reply("Ads, Spamfilters, Captcha, Adds whitespace, Slow, Ugly, No comment/fork/annotate, Breaks copy/paste, Blocked for some people, etc. See !pastebin.com")
+
+    def on_help(self):
+        return "Usage: pastebin.com|badpastebin"
