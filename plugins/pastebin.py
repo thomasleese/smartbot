b3dce3b2fc1bb9e1823237cf24b2c5fdd0658f95
Tom Leese <tom@tomleese.me.uk>
2014-05-19 19:06:39 +0100

    Refactor pastebin plugin

--- plugins/pastebin.py
+++ plugins/pastebin.py
@@ -9,9 +9,10 @@ class Plugin:
     names = ["pastebin.com", "badpastebin"]
 
     def on_command(self, bot, msg, stdin, stdout, reply):
-        if msg["args"][0] == "pastebin.com":
+        arg0 = msg["args"][0]
+        if arg0 == "pastebin.com":
             reply("Don't use it. Use some sane pastebin like bpaste.net, gist.github.com, sprunge.us or ix.io. Also see !badpastebin")
-        elif msg["args"][0] == "badpastebin":
+        elif arg0 == "badpastebin":
             reply("Ads, Spamfilters, Captcha, Adds whitespace, Slow, Ugly, No comment/fork/annotate, Breaks copy/paste, Blocked for some people, etc. See !pastebin.com")
 
     def on_help(self):
