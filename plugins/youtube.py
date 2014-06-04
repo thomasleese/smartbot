107c9584d528cd1036fac97996dd779ede0e62ca
Tom Leese <tom@tomleese.me.uk>
2014-06-04 19:01:37 +0100

    Add ISO8601 time interval parser, close #34
    Also improve code formatting of YouTube plugin.

--- plugins/youtube.py
+++ plugins/youtube.py
@@ -1,45 +1,52 @@
 import re
-import requests
 import urllib
+
+import isodate
+import requests
 
 from smartbot import utils
 from smartbot.formatting import *
+
+
+REGEX = r"(?:https?://)?(?:www\.)?youtu\.?be(?:\.com)?/(?:watch\?v=)?([^\s]+)"
 
 
 class Plugin:
     def __init__(self, key):
         self.key = key
 
+    def _get_reply(self, bot, i, video):
+        channelTitle = video["snippet"]["channelTitle"]
+        duration = isodate.parse_duration(video["contentDetails"]["duration"])
+        views = video["statistics"]["viewCount"]
+        likes = video["statistics"]["likeCount"]
+        dislikes = video["statistics"]["dislikeCount"]
+        thumbnail = video["snippet"]["thumbnails"]["default"]["url"]
+
+        format_string = "{}: {} | {} | {} {} {} | {}"
+        return format_string.format(bot.format("[{}]".format(i), Style.bold),
+                                    channelTitle,
+                                    duration,
+                                    views,
+                                    bot.format(likes, Colour.fg_green),
+                                    bot.format(dislikes, Colour.fg_red),
+                                    bot.format(thumbnail, Colour.fg_grey))
+
     def on_message(self, bot, msg, reply):
-        match = re.findall(r"(?:https?://)?(?:www\.)?youtu\.?be(?:\.com)?/(?:watch\?v=)?([^\s]+)", msg["message"], re.IGNORECASE)
-        for i, id in enumerate(match):
-            url = "https://www.googleapis.com/youtube/v3/videos?key={}&part=contentDetails%2Csnippet%2Cstatistics&id={}".format(
-                urllib.parse.quote(self.key),
-                urllib.parse.quote(id)
-            )
+        match = re.findall(REGEX, msg["message"], re.IGNORECASE)
+        for i, video_id in enumerate(match):
+            url = "https://www.googleapis.com/youtube/v3/videos"
             headers = {"User-Agent": "SmartBot"}
             payload = {
                 "key": self.key,
-                "id": id,
+                "id": video_id,
                 "part": ",".join(["contentDetails", "snippet", "statistics"]),
             }
 
-            res = requests.get(url, headers=headers).json()
+            res = requests.get(url, headers=headers, params=payload).json()
             if res["items"]:
                 video = res["items"][0]
-                channelTitle = video["snippet"]["channelTitle"]
-                duration = video["contentDetails"]["duration"]
-                views = video["statistics"]["viewCount"]
-                likes = video["statistics"]["likeCount"]
-                dislikes = video["statistics"]["dislikeCount"]
-                thumbnail = video["snippet"]["thumbnails"]["default"]["url"]
-                reply("{}: {} | {} | {} {} {} | {}".format(bot.format("[{}]".format(i), Style.bold),
-                                                           channelTitle,
-                                                           duration,
-                                                           views,
-                                                           bot.format(likes, Colour.fg_green),
-                                                           bot.format(dislikes, Colour.fg_red),
-                                                           bot.format(thumbnail, Colour.fg_grey)))
+                reply(self._get_reply(bot, i, video))
 
     def on_help(self):
         return "Sends information about YouTube URLs posted."
