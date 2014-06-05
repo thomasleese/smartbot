885a77871b86ecf7e5fb97d35c06ffade6d78804
Tom Leese <tom@tomleese.me.uk>
2014-06-05 11:52:52 +0100

    Improve github plugin

--- plugins/github.py
+++ plugins/github.py
@@ -1,41 +1,22 @@
-import io
-import unittest
-import requests
+from smartbot import utils
+from smartbot.exceptions import *
+from smartbot.formatting import *
 
 
 class Plugin:
+    STATUS_URL = "https://status.github.com/api/last-message.json"
+
     def on_command(self, bot, msg, stdin, stdout, reply):
         if len(msg["args"]) >= 2:
             action = msg["args"][1]
             if "status".startswith(action):
-                url = "https://status.github.com/api/last-message.json"
-                headers = {"User-Agent": "SmartBot"}
-
-                res = requests.get(url, headers=headers).json()
+                session = utils.web.requests_session()
+                res = session.get(Plugin.STATUS_URL).json()
                 print("{0} - {1}".format(res["created_on"], res["body"]), file=stdout)
             else:
-                print("No such action:", action, file=stdout)
+                raise StopCommandWithHelp(self)
         else:
-            print(self.on_help(), file=stdout)
+            raise StopCommandWithHelp(self)
 
     def on_help(self):
-        return "Usage: github status"
-
-
-class Test(unittest.TestCase):
-    def setUp(self):
-        self.plugin = Plugin()
-
-    def test_status(self):
-        stdout = io.StringIO()
-        self.plugin.on_command(None, {"args": [None, "status"]}, None, stdout, None)
-        self.assertFalse(stdout.getvalue().startswith("No such action"))
-        self.assertNotEqual(self.plugin.on_help(), stdout.getvalue().strip())
-
-    def test_help(self):
-        self.assertTrue(self.plugin.on_help())
-
-    def test_no_args(self):
-        stdout = io.StringIO()
-        self.plugin.on_command(None, {"args": [None]}, stdout, stdout, None)
-        self.assertEqual(self.plugin.on_help(), stdout.getvalue().strip())
+        return "{} status".format(self.bot.format("github", Style.bold))
