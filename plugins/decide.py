bfd2cee602ec7cfd3550fc39ac282821e97d9fd2
Tom Leese <tom@tomleese.me.uk>
2014-06-05 11:44:47 +0100

    Improve help for decide plugin

--- plugins/decide.py
+++ plugins/decide.py
@@ -1,39 +1,27 @@
-import io
 import random
 import shlex
-import unittest
+
+from smartbot.exceptions import *
+from smartbot.formatting import *
 
 
 class Plugin:
+    """
+    Save your brain for more important things by letting SmartBot make
+    important decisions for you.
+    """
     def on_command(self, bot, msg, stdin, stdout, reply):
         args = msg["args"][1:]
         if not args:
             args = shlex.split(stdin.read().strip())
-        if args:
-            print(random.choice(args), file=stdout)
-        else:
-            print(self.on_help(), file=stdout)
+
+        if not args:
+            raise StopCommandWithHelp(self)
+
+        print(random.choice(args), file=stdout)
 
     def on_help(self):
-        return "Usage: decide <a> <b> …"
-
-
-class Test(unittest.TestCase):
-    def setUp(self):
-        self.plugin = Plugin()
-
-    def test_decisions(self):
-        decisions = []
-        for i in range(1, 10):
-            decisions.append("decision {0}".format(i))
-            stdout = io.StringIO()
-            self.plugin.on_command(None, {"args": [None] + decisions}, None, stdout, None)
-            self.assertTrue(stdout.getvalue().strip() in decisions)
-
-    def test_help(self):
-        self.assertTrue(self.plugin.on_help())
-
-    def test_no_args(self):
-        stdout = io.StringIO()
-        self.plugin.on_command(None, {"args": [None]}, stdout, stdout, None)
-        self.assertEqual(self.plugin.on_help(), stdout.getvalue().strip())
+        return "{} {}…".format(
+            self.bot.format("decide", Style.bold),
+            self.bot.format("option", Style.underline)
+        )
