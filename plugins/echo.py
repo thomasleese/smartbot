42066e99ee12007baa79f7594999174d5c49bc7b
Tom Leese <tom@tomleese.me.uk>
2014-06-05 11:46:59 +0100

    Improve echo plugin

--- plugins/echo.py
+++ plugins/echo.py
@@ -1,25 +1,12 @@
-import io
-import unittest
-
+from smartbot.formatting import *
 
 class Plugin:
+    """Reflect on what you say by getting SmartBot to echo it back at you."""
     def on_command(self, bot, msg, stdin, stdout, reply):
         print(*msg["args"][1:], file=stdout)
 
     def on_help(self):
-        return "Usage: echo <string>"
-
-
-class Test(unittest.TestCase):
-    def setUp(self):
-        self.plugin = Plugin()
-
-    def test_words(self):
-        for i in range(1, 100):
-            stdout = io.StringIO()
-            words = [None] + ["word"] * i
-            self.plugin.on_command(None, {"args": words}, None, stdout, None)
-            self.assertEqual(stdout.getvalue().strip(), " ".join(words[1:]))
-
-    def test_help(self):
-        self.assertTrue(self.plugin.on_help())
+        return "{} {}".format(
+            self.bot.format("echo", Style.bold),
+            self.bot.format("something", Style.underline)
+        )
