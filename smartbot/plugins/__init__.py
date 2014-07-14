ce3e8d3e1f5a47d4b773819590088e908e1e6136
Tom Leese <tom@tomleese.me.uk>
2014-07-14 20:48:49 +0100

    Add Instagram and Vimeo plugins from master

--- smartbot/plugins/__init__.py
+++ smartbot/plugins/__init__.py
@@ -1,6 +1,6 @@
 from . import ach, autojoin, brfares, complete, crypto, decide, echo, \
               freebase, github, google, grep, hash, help, heroku, http, \
-              humble, isup, joke, news, nicebot, oeis, pastebin, remind, \
-              seen, sendto, sentiments, sprunge, steam, tee, trains, \
-              translate, twitter, unicode, wait, websites, wolfram, xkcd, \
-              youtube
+              humble, instagram, instagram, isup, joke, news, nicebot, oeis, \
+              pastebin, remind, seen, sendto, sentiments, sprunge, steam, tee, \
+              trains, translate, twitter, unicode, vimeo, wait, websites, \
+              wolfram, xkcd, youtube
