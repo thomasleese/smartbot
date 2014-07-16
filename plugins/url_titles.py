eb1f698b226e404575a835aa4792199fd77f0318
Tom Leese <tom@tomleese.me.uk>
2014-07-16 09:33:43 +0100

    Move YouTube URL detector into URL titles plugin

--- plugins/url_titles.py
+++ plugins/url_titles.py
@@ -1,5 +1,7 @@
-import functools
 import re
+import urllib
+
+import isodate
 
 import smartbot
 from smartbot import utils
@@ -7,17 +9,82 @@ from smartbot.exceptions import *
 from smartbot.formatting import *
 
 
+class YouTube:
+    REGEX = r"(https?://)?(www\.)?(youtu\.?be|listenonrepeat)(\.com)?([^\s]+)"
+
+    def __init__(self, key):
+        self.key = key
+
+    @staticmethod
+    def _get_video_id(text):
+        matches = re.findall(YouTube.REGEX, text, re.IGNORECASE)
+        for match in matches:
+            try:
+                url = urllib.parse.urlparse("".join(match))
+                if url.netloc == "youtu.be":
+                    return url.path[1:]
+                elif "v" in url.query:
+                    return urllib.parse.parse_qs(url.query)["v"]
+            except (ValueError, KeyError):
+                pass
+
+    def _get_video_info(self, video_id):
+        url = "https://www.googleapis.com/youtube/v3/videos"
+        payload = {
+            "key": self.key,
+            "id": video_id,
+            "part": ",".join(["contentDetails", "snippet", "statistics"])
+        }
+
+        s = utils.web.requests_session()
+        res = s.get(url, params=payload).json()
+        if res["items"]:
+            return res["items"][0]
+
+    @staticmethod
+    def _get_title(bot, video):
+        title = video["snippet"]["title"]
+        channelTitle = video["snippet"]["channelTitle"]
+        duration = isodate.parse_duration(video["contentDetails"]["duration"])
+        views = video["statistics"]["viewCount"]
+        likes = video["statistics"]["likeCount"]
+        dislikes = video["statistics"]["dislikeCount"]
+
+        return "{} by {} | {} | {} {} {}".format(
+            bot.format(title, Style.underline),
+            bot.format(channelTitle, Style.underline),
+            duration,
+            views,
+            bot.format(likes, Colour.fg_green),
+            bot.format(dislikes, Colour.fg_red)
+        )
+
+    def __call__(self, plugin, url):
+        video_id = self._get_video_id(url)
+        if video_id:
+            video = self._get_video_info(video_id)
+            if video:
+                return self._get_title(plugin.bot, video)
+
+
+class Website:
+    def __call__(self, plugin, url):
+        return utils.web.get_title(url)
+
+
 class Plugin(smartbot.Plugin):
     """Get URL titles."""
     names = ["url_titles"]
 
-    def __init__(self):
+    def __init__(self, youtube_key=None):
         self.handlers = []
-        self.handlers.append(utils.web.get_title)
+        if youtube_key:
+            self.handlers.append(YouTube(youtube_key))
+        self.handlers.append(Website())
 
     def _get_title(self, url):
         for handler in self.handlers:
-            title = handler(url)
+            title = handler(self, url)
             if title:
                 return title
 
